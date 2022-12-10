d5f4d4039d9769099c9f6afcc7dcae8bf1421e73
pro <quddbscjswow@naver.com>
2022-12-10 12:32:33 +0900

    commit

--- src/main/java/com/example/restfulwebservice2/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice2/HelloWorldController.java
@@ -11,8 +11,9 @@ public class HelloWorldController {
         return "Hello World";
     }
 
-    @GetMapping(path = "/hello-world-bean")
+    @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
+
 }
